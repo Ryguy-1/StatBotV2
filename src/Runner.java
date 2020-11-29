10cab20282be60a36634627027c6895d3abdb558
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2020-11-29 00:18:36 -0800

    Re-Push

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,6 @@
 import javax.security.auth.login.LoginException;
 
 //hidden class in public version
-//git commit test 1 desktop
-//git commit test 2 laptop
 public class Runner {
 	private final String discordAPIKey = "Nzc5MTg1MTM3OTcxNDk0OTMy.X7c2tQ.UXkX1wWybGeoEK5JOMX3Pqgu7y4";
 
